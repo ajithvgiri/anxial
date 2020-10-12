5d93b7c2e671def285f1a862a37165dc90f7413e
ajithvgiri <mail.ajithvgiri@gmail.com>
2020-10-12 14:08:46 +0530

    feature(initial):
    initial commit

---
+++ app/src/main/java/com/ajithvgiri/anxial/data/model/LoggedInUser.kt
@@ -0,0 +1,9 @@
+package com.ajithvgiri.anxial.data.model
+
+/**
+ * Data class that captures user information for logged in users retrieved from LoginRepository
+ */
+data class LoggedInUser(
+        val userId: String,
+        val displayName: String
+)
---
+++ app/src/main/java/com/ajithvgiri/anxial/ui/login/LoggedInUserView.kt
@@ -0,0 +1,9 @@
+package com.ajithvgiri.anxial.ui.login
+
+/**
+ * User details post authentication that is exposed to the UI
+ */
+data class LoggedInUserView(
+        val displayName: String
+        //... other data fields that may be accessible to the UI
+)
